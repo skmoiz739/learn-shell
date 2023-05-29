3901d67ff8468c9b0976a94e557b1d6ed3649d93
Moeez Shaik <skmoiz739@gmail.com>
2023-05-29 03:58:25 -0400

    nginx server

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,16 @@
 echo -e "\e[33m installing nginx \e[0m
 yum install nginx -y &>>/tmp/roboshop.log
+
 echo -e "\e[33m removing files \e[0m
 rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
+
 echo -e "\e[33m downloading files \e[0m
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
+
 echo -e "\e[33m extract files \e[0m
 cd /usr/share/nginx/html &>>/tmp/roboshop.log
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
+
 echo -e "\e[33m start nginx \e[0m
 systemctl enable nginx &>>/tmp/roboshop.log
 systemctl start nginx  &>>/tmp/roboshop.log
